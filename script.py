fd730c6b30504e0910d62d238d3f5dca0f5350a4
fernandomsilva <fernandomsilva@nyu.edu>
2016-04-17 17:50:11 -0400

    Visualization of the map

--- script.py
+++ script.py
@@ -1,12 +1,13 @@
 from tickettoride import *
 from loadgraphfile import *
 from loaddestinationdeck import *
-
-
+from Visualization import *
 
 game_object = Game(Board(loadgraphfromfile('usa.txt')), point_table(), loaddestinationdeckfromfile('usa_destinations.txt'), make_train_deck(12, 14), [Player([], 45, 0), Player([], 45, 0)], 0)
 game_object.setup()
-gh = GameHandler(game_object, [AStarAgent(), RAStarAgent()])
+#gh = GameHandler(game_object, [AStarAgent(), RAStarAgent()])
+gh = GameHandler(game_object, [CopyAgent(), CopyAgent()])
 gh.play()
+show_graph(game_object.board.graph)
 #g.choose_destination_cards(0, [g.players[0].hand[-1], g.players[0].hand[-2]])
 #g.choose_destination_cards(1, [g.players[1].hand[-1], g.players[1].hand[-2]])
