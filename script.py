2f8413eb14d087af7a199b5f57d8e8f59262db92
Scott <scottlee27@hotmail.com>
2016-04-18 15:14:58 -0400

    Changes to A*

--- script.py
+++ script.py
@@ -4,9 +4,9 @@ from loaddestinationdeck import *
 
 
 
-game_object = Game(Board(loadgraphfromfile('usa.txt')), point_table(), loaddestinationdeckfromfile('usa_destinations.txt'), make_train_deck(12, 14), [Player([], 45, 0), Player([], 45, 0)], 0)
+game_object = Game(Board(loadgraphfromfile('usa.txt')), point_table(), loaddestinationdeckfromfile('usa_destinations.txt'), make_train_deck(12, 14), [Player([], 45, 0), Player([], 45, 0), Player([], 45, 0)], 0)
 game_object.setup()
-gh = GameHandler(game_object, [AStarAgent(), RAStarAgent()])
+gh = GameHandler(game_object, [AStarAgent(), RAStarAgent(), Agent()])
 gh.play()
 #g.choose_destination_cards(0, [g.players[0].hand[-1], g.players[0].hand[-2]])
 #g.choose_destination_cards(1, [g.players[1].hand[-1], g.players[1].hand[-2]])
