665265a6db6e10c586e09889dca4a9020e337d45
Scott <scottlee27@hotmail.com>
2016-04-30 19:47:20 -0400

    chaged goloader to a data aggregator

--- goloader.py
+++ goloader.py
@@ -1,18 +1,53 @@
 import pickle
+import os
 
-filename = 'testdata.go'
-f = open(filename, 'rb')
-flist = []
-try:
-	while True:
-		flist = pickle.load(f)
-except:
-	pass
+def aggregateData(folder):
+	gameobjects = []
+	for (dirname, dirs, files) in os.walk(folder):
+		for filename in files:
+			print 'filename'
+			if filename.endswith('.go'):
+				filename = 'testdata.go'
+				f = open(filename, 'rb')
+				flist = []
+				try:
+					while True:
+						flist = pickle.load(f)
+				except:
+					pass
 
-print flist
+				print flist
 
-print flist.players[1].points
-print flist.players[0].hand
-flist.players[0].print_destination_cards()
-print flist.board.graph.nodes()
-flist.printScoring(1)
+				gameobjects.append(flist)
+
+	pscores = []
+	for x in range(0, gameobjects[0].number_of_players):
+		pscores.append([])
+	
+	for go in gameobjects:
+		for p in range(0, go.number_of_players):
+			pscores[p].append(go.players[p].points)
+
+	print 'averages:'
+	pnum = 1
+	for scores in pscores:
+		print 'player' + str(pnum) + ': ' + str(sum(scores) / len(scores))
+		pnum = pnum + 1
+	
+	print ''
+
+	wins = [0] * len(pscores)
+	print 'winratios:'
+	for go in gameobjects:
+		best = -500
+		bplayer = -1
+		for p in range(0, go.number_of_players):
+			if go.players[p].points > best:
+				bplayer = p
+				best = go.players[p].points
+		wins[bplayer] = wins[bplayer] + 1
+
+	pnum = 1
+	for wincount in wins:
+		print 'player' + str(pnum) + ': ' + str(wincount) + '/' + str(len(gameobjects))
+		pnum = pnum + 1
